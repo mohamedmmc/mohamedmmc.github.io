4f018ce89c9d0f3812cfd988c8b6bff21d1eb0fc
Mohamed Melek Chtourou <mohamedmelekchtourou@MacBook-Air.local>
2025-10-25 15:12:40 +0100

    test

--- lib/core/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const background = Colors.black;
-  static const accent = Colors.cyanAccent;
-  static const textPrimary = Colors.white;
-  static const textSecondary = Colors.white70;
-  static const buttonBackground = Color(0xFF0A0A0A);
-}
--- lib/core/app_values.dart
+++
@@ -1,5 +0,0 @@
-class AppValues {
-  static const double padding = 16.0;
-  static const double buttonSpacing = 20.0;
-  static const double sectionSpacing = 32.0;
-}
